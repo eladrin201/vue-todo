613beb86c759890dcbc18a525e7df21dc565e50b
Allison Betancourt <allisonbbetancourt@gmail.com>
2018-06-10 20:53:37 -0500

    list in data will display on screen with complete/completed status showing

---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <ul id="todos">
+      <li class="ui centered card" v-for="(todo, key) in todos" :key="key" v-if="todo">
+        <div class="content">
+          <h2 class="header">
+            {{ todo.title }}
+            <small>
+              {{ todo.project }}
+            </small>
+          </h2>
+          <div class="extra content">
+            <button class="right floated edit icon">
+              <i class="edit icon"></i>
+              <span class="sr-only">Edit</span>
+            </button>
+            <button class="ui bottom attached green basic button" v-show="todo.done">
+              Completed
+            </button>
+            <button class="ui bottom attached red basic button" v-show="!todo.done">
+              Complete
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: ['todos'],
+};
+</script>
+
+<style media="screen">
+  h2 small {
+    display: block;
+  }
+</style>
